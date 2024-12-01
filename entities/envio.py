246dcb6ee6df4cbebee5fb4e4de2ecac3b125bfc
Blasco <luxinthehouse1477@gmail.com>
2024-12-01 14:40:26 -0700

    Se agregó la funcion de envios en el header, asi como la entidad envio

---
+++ entities/envio.py
@@ -0,0 +1,20 @@
+from persistence.db import get_db_connection
+from mysql.connector import Error
+
+class Envio:
+    def __init__(self, nombre, codigo):
+        self.nombre = nombre 
+        self.codigo = codigo
+    
+    @classmethod
+    def get_all(cls):
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute('SELECT * FROM ciudad')
+            return cursor.fetchall()
+        except Error as e:
+            return str(e)
+        finally:
+            cursor.close()
+            connection.close() 
