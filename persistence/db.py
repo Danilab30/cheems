97fa6cc4f01182367f905bb6d0c9786655324fb5
Blasco <luxinthehouse1477@gmail.com>
2024-12-05 15:49:43 -0700

    Se modifico las credenciales a la base de datos

--- persistence/db.py
+++ persistence/db.py
@@ -3,8 +3,8 @@ from mysql.connector import Error
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='marioo246',
-        database='cheems'
+        host='162.241.2.39',
+        user='itsonapp_244442',
+        password='244442db#531R',
+        database='itsonapp_244442'
     )
